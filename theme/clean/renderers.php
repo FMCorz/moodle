afbd1db09d013ad6995781579fc001dc1200a082
Frederic Massart <fred@moodle.com>
2013-07-24 18:51:10 +0800

    Forcing sub header with activity name

--- theme/clean/renderers.php
+++ theme/clean/renderers.php
@@ -27,7 +27,59 @@ class theme_clean_block_navigation_renderer extends block_navigation_renderer {
 
 }
 
+require_once($CFG->dirroot . '/mod/lesson/renderer.php');
+class theme_clean_mod_lesson_renderer extends mod_lesson_renderer {
+    public function header() {
+        return '';
+    }
+}
+
+require_once($CFG->dirroot . '/mod/assign/renderer.php');
+class theme_clean_mod_assign_renderer extends mod_assign_renderer {
+    /**
+     * Render the header.
+     *
+     * @param assign_header $header
+     * @return string
+     */
+    public function render_assign_header(assign_header $header) {
+        $o = '';
+
+        if ($header->subpage) {
+            $this->page->navbar->add($header->subpage);
+        }
+
+        $this->page->set_title(get_string('pluginname', 'assign'));
+        $this->page->set_heading($header->assign->name);
+
+        $o .= $this->output->header();
+        if ($header->preface) {
+            $o .= $header->preface;
+        }
+        // $heading = format_string($header->assign->name, false, array('context' => $header->context));
+        // $o .= $this->output->heading($heading);
+
+        if ($header->showintro) {
+            $o .= $this->output->box_start('generalbox boxaligncenter', 'intro');
+            $o .= format_module_intro('assign', $header->assign, $header->coursemoduleid);
+            $o .= $this->output->box_end();
+        }
+
+        return $o;
+    }
+}
+
 class theme_clean_core_renderer extends theme_bootstrapbase_core_renderer {
+
+    public function main_content() {
+        $html = '<div role="main">';
+        if ($this->page->cm) {
+            $html .= $this->heading($this->page->cm->name, 2);
+        }
+        $html .= $this->unique_main_content_token;
+        $html .= '</div>';
+        return $html;
+    }
 
     /*
      * This renders the navbar.
