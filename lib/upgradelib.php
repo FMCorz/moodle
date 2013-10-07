8506764a96cf59ba1cfd4f999b28bef2155950e4
Frederic Massart <fred@moodle.com>
2013-10-07 18:15:45 +0800

    MDL-42014 cache: Force update of default stores on upgrade

--- lib/upgradelib.php
+++ lib/upgradelib.php
@@ -1549,7 +1549,9 @@ function upgrade_core($version, $verbose) {
         external_update_descriptions('moodle');
         events_update_definition('moodle');
         message_update_providers('moodle');
-        // Update core definitions.
+
+        // Update core definitions and default stores.
+        cache_helper::update_default_stores();
         cache_helper::update_definitions(true);
 
         // Purge caches again, just to be sure we arn't holding onto old stuff now.
