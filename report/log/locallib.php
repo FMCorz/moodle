a8d0fac09d7e803c73491b7bf1cc03835170bc22
Frederic Massart <fred@moodle.com>
2016-06-22 15:28:16 +0800

    report

--- report/log/locallib.php
+++ report/log/locallib.php
@@ -57,6 +57,112 @@ function report_log_print_graph($course, $userid, $type, $date=0, $logreader='')
         return array();
     }
 
+    // HACK.
+    global $CFG, $DB, $OUTPUT, $PAGE;
+    $timenow = time();
+    $logs = [];
+    $user = $DB->get_record("user", array("id" => $userid, 'deleted' => 0), '*', MUST_EXIST);
+
+    $site = get_site();
+    if ($course->id == $site->id) {
+        $courseselect = 0;
+    } else {
+        $courseselect = $course->id;
+    }
+
+    $maxseconds = REPORT_LOG_MAX_DISPLAY * 3600 * 24;  // Seconds.
+    if ($timenow - $course->startdate > $maxseconds) {
+        $course->startdate = $timenow - $maxseconds;
+    }
+
+    if (!empty($CFG->loglifetime)) {
+        $maxseconds = $CFG->loglifetime * 3600 * 24;  // Seconds.
+        if ($timenow - $course->startdate > $maxseconds) {
+            $course->startdate = $timenow - $maxseconds;
+        }
+    }
+
+    $timestart = $coursestart = usergetmidnight($course->startdate);
+    if ((($timenow - $timestart) / 86400.0) > 40) {
+        $reducedays = 7;
+    } else {
+        $reducedays = 0;
+    }
+
+    $days = array();
+    $i = 0;
+    while ($timestart < $timenow) {
+        $timefinish = $timestart + 86400;
+        if ($reducedays) {
+            // if ($i % $reducedays) {
+                // $days[$i] = "";
+            // } else {
+                $days[$i] = userdate($timestart, "%a %d %b");
+            // }
+        } else {
+            $days[$i] = userdate($timestart, "%a %d %b");
+        }
+        $logs[$i] = 0;
+        $i++;
+        $timestart = $timefinish;
+    }
+
+    $rawlogs = report_log_usercourse($user->id, $courseselect, $coursestart, $logreader);
+    foreach ($rawlogs as $rawlog) {
+        $logs[$rawlog->day] = $rawlog->num;
+    }
+
+    $chart = new core\chart_bar();
+    $series = new core\chart_series('Hits', $logs);
+    $chart->add_series($series);
+    $chart->set_labels($days);
+    echo $OUTPUT->render($chart);
+    return;
+    // END HACK.
+
+    $graph = new graph(750, 400);
+
+    if (empty($rawlogs)) {
+        $graph->parameter['y_axis_gridlines'] = 2;
+        $graph->parameter['y_max_left'] = 1;
+    }
+
+    $a = new stdClass();
+    $a->coursename = format_string($course->shortname, true, array('context' => $coursecontext));
+    $a->username = fullname($user, true);
+    $graph->parameter['title'] = get_string("hitsoncourse", "", $a);
+
+    $graph->x_data           = $days;
+
+    $graph->y_data['logs']   = $logs;
+    $graph->y_order = array('logs');
+
+    // Make sure the Y-axis gridlines correspond to the integer values.
+    if (count($logs) && ($ymax = max($logs)) && ($graph->parameter['y_axis_gridlines'] > 1)) {
+        if ($ymax < $graph->parameter['y_axis_gridlines'] - 1) {
+            $graph->parameter['y_axis_gridlines'] = $ymax + 1;
+        } else if ($ymax % ($graph->parameter['y_axis_gridlines'] - 1)) {
+            $graph->parameter['y_max_left'] = $graph->parameter['y_max_right'] =
+                ceil($ymax/($graph->parameter['y_axis_gridlines'] - 1)) * ($graph->parameter['y_axis_gridlines'] - 1);
+        }
+    }
+
+    if (!empty($CFG->preferlinegraphs)) {
+        $graph->y_format['logs'] = array('colour' => 'blue', 'line' => 'line');
+    } else {
+        $graph->y_format['logs'] = array('colour' => 'blue', 'bar' => 'fill', 'bar_size' => 0.6);
+        $graph->parameter['bar_spacing'] = 0;
+    }
+
+
+    $graph->parameter['y_label_left'] = get_string("hits");
+    $graph->parameter['label_size'] = "12";
+    $graph->parameter['x_axis_angle'] = 90;
+    $graph->parameter['x_label_angle'] = 0;
+    $graph->parameter['tick_length'] = 0;
+    $graph->parameter['shadow'] = 'none';
+
+
     $url = new moodle_url('/report/log/graph.php', array('id' => $course->id, 'user' => $userid, 'type' => $type,
         'date' => $date, 'logreader' => $logreader));
     echo html_writer::empty_tag('img', array('src' => $url, 'alt' => ''));
