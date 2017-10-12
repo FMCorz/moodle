4c87fafa182098e20e40941d716afeb3e236ec3c
Frédéric Massart <fred@branchup.tech>
2017-10-12 17:56:11 +0800

    MDL-56453 core_completion: Option for successful activity completion

--- lib/db/upgrade.php
+++ lib/db/upgrade.php
@@ -2961,5 +2961,22 @@ function xmldb_main_upgrade($oldversion) {
         upgrade_main_savepoint(true, 2017051502.04);
     }
 
+    if ($oldversion < 2017051502.14) {
+
+        // Default value being set is the same value as completion_criteria_activity::STATUS_COMPLETED.
+        // Define field modulestatus to be added to course_completion_criteria.
+        $table = new xmldb_table('course_completion_criteria');
+        $field = new xmldb_field('modulestatus', XMLDB_TYPE_INTEGER, '10', null, null, null, '1', 'moduleinstance');
+
+        // Conditionally launch add field modulestatus.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Main savepoint reached.
+        upgrade_main_savepoint(true, 2017051502.14);
+    }
+
+
     return true;
 }
